e58a92e2b7dfe637569db1839d55422d4d3c3499
William Grzybowski <wg@FreeBSD.org>
2013-09-02 11:42:24 -0300

    Make sure the nested resources are authenticated

--- gui/api/resources.py
+++ gui/api/resources.py
@@ -234,6 +234,7 @@ class VolumeResourceMixin(object):
         ]
 
     def _get_parent(self, request, kwargs):
+        self.is_authenticated(request)
         try:
             bundle = self.build_bundle(
                 data={'pk': kwargs['pk']}, request=request
